a3738da38c4b24390e363ef1faf42b686cd10e5e
rishueee <jhapinki567@gmail.com>
2024-03-22 16:21:15 +0530

    dashboard changes made

--- client/src/component/index.js
+++ client/src/component/index.js
@@ -1,4 +1,13 @@
 export { default as Login } from "./Login";
 export { default as Home } from "./Home";
 export { default as Header } from "./Header";
+
+
+
+// DASHBOARD
 export { default as Dashboard } from "./Dashboard";
+export {default as DashboardHome} from "./DashboardHome";
+export {default as DashboardUsers} from "./DashboardUsers";
+export {default as DashboardSongs} from "./DashboardSongs";
+export {default as DashboardArtist} from "./DashboardArtist";
+export {default as DashboardAlbum} from "./DashboardAlbum";
